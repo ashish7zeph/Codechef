2151af0fd427e3420ebb1b1790a7ebb49618d9cc
Ashish Pandey <31332047+ashish7zeph@users.noreply.github.com>
2018-05-26 00:00:43 +0530

    Delete chef_and_fruits.py

--- chef_and_fruits.py
+++
@@ -1,6 +0,0 @@
-from math import fabs
-for _ in range(int(input())):
-    n, m, k = [int(x) for x in input().split()]
-    diff = fabs(n - m)
-    if diff > k: print(int(diff - k))
-    else: print(0)
