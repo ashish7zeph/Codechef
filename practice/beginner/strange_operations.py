028cd4e9efa3f845d3a6e279941b2d4387351483
Ashish Pandey <31332047+ashish7zeph@users.noreply.github.com>
2018-05-28 19:10:18 +0530

    Update strange_operations.py

--- practice/beginner/strange_operations.py
+++ practice/beginner/strange_operations.py
@@ -1,6 +1,6 @@
 for _ in range(int(input())):
     n, k = [int(x) for x in input().split()]
     a = [int(x) for x in input().split()]
-    s = sum(a)
+    s = sum(a) + 1
     if s % 2 and k == 1: print('odd')
     else: print('even')
