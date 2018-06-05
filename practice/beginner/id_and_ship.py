8408c361abad88d6b02ed533dfccc8d4db27e6cf
Ashish Pandey <31332047+ashish7zeph@users.noreply.github.com>
2018-06-05 16:12:51 +0530

    Add files via upload

---
+++ practice/beginner/id_and_ship.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    id = input().lower()
+    if id == 'b': print('BattleShip')
+    elif id == 'c': print('Cruiser')
+    elif id == 'd': print('Destroyer')
+    elif id == 'f': print('Frigate')
